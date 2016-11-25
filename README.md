2ed6414b35d412d24c7411c4e1f26e18f70b3f39
André Bauer <abauer@magix.net>
2016-11-25 15:18:27 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # packager.io-dockerhub-bridge
+
+The webhooks on packager.io unfortunately dont work to trigger docker image builds on hub.docker.com.
+
+Thsi script is a bridge / wrapper to achieve this goal anyway.
+
+Just configure packager.io to access this script and the configured dockerhub webhook will trigger the build.
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'config.php';
+
+$data = array("build" => "true");
+$data_string = json_encode($data);
+
+$ch = curl_init($dockerhub_hook_url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
+
+echo $result;
+
+?>
